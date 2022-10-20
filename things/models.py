5c487a1abd86acdfe8f119a6913c45913a762977
Miron Bykov <miron.bykov@kcl.ac.uk>
2022-10-20 15:43:35 +0100

    Complete excersice 2, attempt 4

--- things/models.py
+++ things/models.py
@@ -3,15 +3,8 @@ from django.db import models
 from django.forms import IntegerField
 
 class Thing(models.Model):
-    name = models.CharField(
-        max_length=30,
-    )
+    name = models.CharField(max_length=30)
 
-    description = models.CharField(
-    
-        max_length=520,
-    )
+    description = models.CharField(max_length=520)
 
-    quantity = IntegerField(
-
-    )
+    quantity = models.IntegerField()
