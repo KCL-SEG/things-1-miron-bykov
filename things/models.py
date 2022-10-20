f9f1c5714a8b7661a91ab687f71a5aa0140036ae
Miron Bykov <miron.bykov@kcl.ac.uk>
2022-10-20 15:05:25 +0100

    Excersice 2 complete, attempt 1

--- things/models.py
+++ things/models.py
@@ -1,1 +1,22 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.forms import IntegerField
+
+class Thing(AbstractUser):
+    name = models.ChardField(
+        unique=True,
+        blank=False,
+        max_length=30,
+    )
+
+    description = models.CharField(
+        unique=False,
+        blank=True,
+        max_length=520,
+    )
+
+    quantity = IntegerField(
+        unique=False,
+        blank=True,
+    )
