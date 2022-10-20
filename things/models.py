1fbbcbd935cb42d8f4e07b99f5415f3a62cf9264
Miron Bykov <miron.bykov@kcl.ac.uk>
2022-10-20 15:36:57 +0100

    Complete excersice 2, attempt 3

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.forms import IntegerField
 
 class Thing(models.Model):
-    name = models.ChardField(
+    name = models.CharField(
         unique=True,
         blank=False,
         max_length=30,
